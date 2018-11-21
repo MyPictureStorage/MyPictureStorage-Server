874e7deb5143138c8eadf5b990e786ea3f7f320b
KeonHeeLee <beta1360@naver.com>
2018-11-21 21:49:43 +0900

    (+) add #2: init server setting

---
+++ frontend/src/main/java/io/github/keonheelee/mypicturestorage/ServletInitializer.java
@@ -0,0 +1,13 @@
+package io.github.keonheelee.mypicturestorage;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyPictureStorageApplication.class);
+	}
+
+}
